2b9cab6d2e90011cef5651077042560a97e75f6c
giovane <giovane.tomaz@redealumni.com>
2016-05-22 00:36:39 -0300

    Funcionando porra

--- client/app/app.component.ts
+++ client/app/app.component.ts
@@ -1,8 +1,23 @@
 import { Component } from '@angular/core';
 
+import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from '@angular/router-deprecated';
+
+import { DashboardComponent }  from './components/dashboard.component';
+
 @Component({
   selector: 'my-app',
-  template: '<h1>My First Angular 2 App</h1>'
+  templateUrl: 'templates/main.html',
+  directives: [ROUTER_DIRECTIVES],
+  providers: [
+    ROUTER_PROVIDERS,
+    //HeroService,
+  ]
 })
+
+@RouteConfig([
+  { path: '/dashboard',  name: 'Dashboard',  component: DashboardComponent, useAsDefault: true },
+  //{ path: '/detail/:id', name: 'HeroDetail', component: HeroDetailComponent },
+  //{ path: '/heroes',     name: 'Heroes',     component: HeroesComponent }
+])
 
 export class AppComponent { }
--- client/systemjs.config.js
+++ client/systemjs.config.js
@@ -8,11 +8,12 @@
     'app':                        'app', // 'dist',
     '@angular':                   'node_modules/@angular',
     'angular2-in-memory-web-api': 'node_modules/angular2-in-memory-web-api',
-    'rxjs':                       'node_modules/rxjs'
+    'rxjs':                       'node_modules/rxjs',
+    'moment':                     'node_modules/moment/moment.js'
   };
   // packages tells the System loader how to load when no filename and/or no extension
   var packages = {
-    'app':                        { main: 'js/main.js',  defaultExtension: 'js' },
+    'app':                        { main: 'js/main.js',  defaultExtension: 'js', format: 'register'},
     'rxjs':                       { defaultExtension: 'js' },
     'angular2-in-memory-web-api': { defaultExtension: 'js' },
   };
